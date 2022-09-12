a490fb67fedebad7efc58434005a2e773a389309
Bruno Alves de Araujo <88730176+brunodev21@users.noreply.github.com>
2022-09-12 15:31:54 -0300

    primeiro commit

---
+++ exercicio2/script.js
@@ -0,0 +1,5 @@
+let inputUser = Number(prompt("Digite a tabuada que deseja ver"))
+for(let i = 1; i < 11; i++){
+    resultado = inputUser * 1
+    console.log(`${inputUser} x ${i} = ${resultado}`)
+}
